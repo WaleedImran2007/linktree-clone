3b789916f80d9b5f668d92d5c6cd83fa87d3d007
Waleed Imran <imrankhalida2009@gmail.com>
2025-08-07 15:22:06 +0500

    Fix suspence issue app/generate/page.js

--- app/generate/page.js
+++ app/generate/page.js
@@ -5,6 +5,7 @@ import { ToastContainer, toast } from 'react-toastify';
 import { useState } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 const Generate = () => {
 
@@ -65,80 +66,83 @@ const Generate = () => {
     }
 
     return (
-        <div className='bg-[#E9C0E9] min-h-[80vh] grid grid-cols-1 lg:grid-cols-2 pt-[120px] px-4 gap-8'>
+        <Suspense>
+            <div className='bg-[#E9C0E9] min-h-[80vh] grid grid-cols-1 lg:grid-cols-2 pt-[120px] px-4 gap-8'>
 
-            {/* Column 1 is for displaying the form and column 2 is for displaying the banner. */}
-            <div className="col1 flex justify-center items-center flex-col w-full lg:w-[130%]">
-                <h1 className='font-bold text-3xl sm:text-4xl text-center'>Create your Linktree</h1>
-                <div className='flex flex-col gap-3 my-8 w-full max-w-xl'>
+                {/* Column 1 is for displaying the form and column 2 is for displaying the banner. */}
+                <div className="col1 flex justify-center items-center flex-col w-full lg:w-[130%]">
+                    <h1 className='font-bold text-3xl sm:text-4xl text-center'>Create your Linktree</h1>
+                    <div className='flex flex-col gap-3 my-8 w-full max-w-xl'>
 
-                    <h2 className='font-semibold text-lg sm:text-xl'>Step 1: Claim your handle</h2>
-                    <div className='mx-0 sm:mx-4'>
-                        <input
-                            value={handle || ""}
-                            onChange={e => { sethandle(e.target.value) }}
-                            className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
-                            type="text"
-                            placeholder='Choose a Handle'
-                        />
+                        <h2 className='font-semibold text-lg sm:text-xl'>Step 1: Claim your handle</h2>
+                        <div className='mx-0 sm:mx-4'>
+                            <input
+                                value={handle || ""}
+                                onChange={e => { sethandle(e.target.value) }}
+                                className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
+                                type="text"
+                                placeholder='Choose a Handle'
+                            />
+                        </div>
+
+                        <h2 className='font-semibold text-lg sm:text-xl'>Step 2: Add Links</h2>
+                        {links && links.map((item, index) => {
+                            return (
+                                <div key={index} className='mx-0 sm:mx-4 flex flex-col sm:flex-row gap-2'>
+                                    <input
+                                        value={item.linktext || ""}
+                                        onChange={e => { handleChange(index, item.link, e.target.value) }}
+                                        className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
+                                        type="text"
+                                        placeholder='Enter link text'
+                                    />
+                                    <input
+                                        value={item.link || ""}
+                                        onChange={e => { handleChange(index, e.target.value, item.linktext) }}
+                                        className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
+                                        type="text"
+                                        placeholder='Enter link'
+                                    />
+                                </div>
+                            )
+                        })}
+                        <button onClick={() => addLink()} className='bg-pink-700 text-white px-4 py-2 rounded-full cursor-pointer w-fit'>+ Add Link</button>
+
+                        <h2 className='font-semibold text-lg sm:text-xl'>Step 3: Add Picture and Description</h2>
+                        <div className='mx-0 sm:mx-4 flex flex-col gap-2'>
+                            <input
+                                value={pic || ""}
+                                onChange={e => { setpic(e.target.value) }}
+                                className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
+                                type="text"
+                                placeholder='Enter link to your picture'
+                            />
+                            <input
+                                value={desc || ""}
+                                onChange={e => { setdesc(e.target.value) }}
+                                className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
+                                type="text"
+                                placeholder='Enter Description'
+                            />
+                            <button
+                                disabled={pic == "" || handle == "" || links[0].linktext == "" || links[0].link == ""}
+                                onClick={() => { submitLinks() }}
+                                className='disabled:bg-slate-600 bg-pink-700 text-white px-4 py-2 rounded-full cursor-pointer w-fit my-4'
+                            >
+                                Create your LinkTree
+                            </button>
+                        </div>
                     </div>
+                </div>
 
-                    <h2 className='font-semibold text-lg sm:text-xl'>Step 2: Add Links</h2>
-                    {links && links.map((item, index) => {
-                        return (
-                            <div key={index} className='mx-0 sm:mx-4 flex flex-col sm:flex-row gap-2'>
-                                <input
-                                    value={item.linktext || ""}
-                                    onChange={e => { handleChange(index, item.link, e.target.value) }}
-                                    className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
-                                    type="text"
-                                    placeholder='Enter link text'
-                                />
-                                <input
-                                    value={item.link || ""}
-                                    onChange={e => { handleChange(index, e.target.value, item.linktext) }}
-                                    className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
-                                    type="text"
-                                    placeholder='Enter link'
-                                />
-                            </div>
-                        )
-                    })}
-                    <button onClick={() => addLink()} className='bg-pink-700 text-white px-4 py-2 rounded-full cursor-pointer w-fit'>+ Add Link</button>
-
-                    <h2 className='font-semibold text-lg sm:text-xl'>Step 3: Add Picture and Description</h2>
-                    <div className='mx-0 sm:mx-4 flex flex-col gap-2'>
-                        <input
-                            value={pic || ""}
-                            onChange={e => { setpic(e.target.value) }}
-                            className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
-                            type="text"
-                            placeholder='Enter link to your picture'
-                        />
-                        <input
-                            value={desc || ""}
-                            onChange={e => { setdesc(e.target.value) }}
-                            className='bg-white px-4 py-2 rounded-3xl focus:outline-pink-500 w-full'
-                            type="text"
-                            placeholder='Enter Description'
-                        />
-                        <button
-                            disabled={pic == "" || handle == "" || links[0].linktext == "" || links[0].link == ""}
-                            onClick={() => { submitLinks() }}
-                            className='disabled:bg-slate-600 bg-pink-700 text-white px-4 py-2 rounded-full cursor-pointer w-fit my-4'
-                        >
-                            Create your LinkTree
-                        </button>
-                    </div>
+                {/* Column 2 - Image */}
+                <div className="col2 w-full h-full flex justify-center items-center">
+                    <img className='w-full max-h-[600px] object-contain' src="/generate.png" alt="" />
+                    <ToastContainer />
                 </div>
             </div>
 
-            {/* Column 2 - Image */}
-            <div className="col2 w-full h-full flex justify-center items-center">
-                <img className='w-full max-h-[600px] object-contain' src="/generate.png" alt="" />
-                <ToastContainer />
-            </div>
-        </div>
+        </Suspense>
     )
 }
 
